95bda32c3989bc01de03806d45e09bf5158f276a
Hussain Ali <iamhussainali2@gmail.com>
2024-02-26 22:32:47 +0530

    added find all donation

--- routes/donationRoutes.js
+++ routes/donationRoutes.js
@@ -7,5 +7,6 @@ router.post("/create-donation", donationController.createDonation)
 router.post("/edit-donation/:id", donationController.editDonationById)
 router.get("/email-donation/:email", donationController.findDonationByEmail)
 router.delete("/delete-donation/:id", donationController.deleteDonationById)
+router.get("/all-donation", donationController.findAlldonation)
 
 module.exports = router;
